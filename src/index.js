cd5420b38d92c0ae3fe3723b44a6ed9e59666f76
Hapsari Laksmi Wijayanti <hapsari@onebyone.io>
2026-07-17 21:01:02 +0800

    implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import express from "express";
+import {matchRouter} from "./routes/matches.js";
 
 const app = express();
 const PORT = 8000;
 
+// Enable middleware to understand JSON data
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from Express server!");
 });
 
+app.use('/matches', matchRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
 
+ 
